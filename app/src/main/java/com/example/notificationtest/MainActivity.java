5dfa882a2d2a1040ae0cab38f4d980b4cac3871e
wenyiYin <443644234@qq.com>
2024-01-24 11:10:35 +0800

    实现通知栏弹出通知效果
    需注意模拟器API版本，测试版本使用的是Pixel 5 API 30

--- app/src/main/java/com/example/notificationtest/MainActivity.java
+++ app/src/main/java/com/example/notificationtest/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.notificationtest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    String ch_id = "channel_1";
+
+    int importance = NotificationManager.IMPORTANCE_HIGH;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button sendNotice = (Button) findViewById(R.id.send_notice);
+        sendNotice.setOnClickListener(this);
+
     }
+
+    @Override
+    public void onClick(View v){
+        int id = v.getId();
+        if(id == R.id.send_notice){
+            NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            NotificationChannel mChannel = new NotificationChannel(ch_id,"test",importance);
+            manager.createNotificationChannel(mChannel);
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this,ch_id);
+            builder.setContentTitle("this is title");
+            builder.setContentText("this is text");
+            builder.setWhen(System.currentTimeMillis());
+            builder.setSmallIcon(R.mipmap.ic_launcher);
+            builder.setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
+            manager.notify(1,builder.build());
+        }
+    }
+
 }
